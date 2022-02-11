7f0392b33ae5f9eba58319406152d101f18dc4ff
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-02-11 14:01:53 -0500

    ported part of main

--- hw-01/src/main.rs
+++ hw-01/src/main.rs
@@ -1,6 +1,15 @@
+use draw_line::Screen;
+use draw_line::Color;
 mod draw_line;
 
 fn main() {
-    // println!("Hello, world!");
-    draw_line::draw_line(0,0,0,0,0,0);
+    let XRES = 500;
+    let YRES = 500;
+    let MAX_COLOR = 255;
+    let mut s = Screen::new(XRES, YRES);
+    let c = Color::newColor(0, 255, 0);
+
+    s.draw_line(0, 0, XRES as i32-1, YRES as i32-1, c);
+    s.draw_line(0, 0, XRES as i32-1, YRES as i32 / 2, c);
+    s.draw_line(XRES as i32-1, YRES as i32-1, 0, YRES as i32 / 2, c);
 }
