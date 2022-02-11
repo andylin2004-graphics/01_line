4193b3f0f75ae8298bb9af154ec4d6ca3bf55707
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-02-11 14:02:50 -0500

    added drop for screen, changed some class types, made some stuff public or mutable

--- hw-01/src/draw_line.rs
+++ hw-01/src/draw_line.rs
@@ -1,116 +1,121 @@
 #[derive(Copy, Clone)]
-struct ColorInfo{
+pub struct Color{
     pub r: i32,
     pub g: i32,
     pub b: i32,
 }
 
-impl ColorInfo{
-    fn new() -> ColorInfo{
-        ColorInfo{r: 0, g:0, b:0}
+impl Color{
+    pub fn new() -> Color{
+        Color{r: 0, g:0, b:0}
     }
 
-    fn to_string(&self) -> String{
+    pub fn newColor(r: i32, g: i32, b: i32) -> Color{
+        Color{r: r, g:g, b:b}
+    }
+
+    pub fn to_string(&self) -> String{
         return format!("{} {} {}", self.r, self.g, self.b);
     }
     
-    fn plotColor(&self, newColor: ColorInfo){
+    pub fn plotColor(&mut self, newColor: Color){
         self.r = newColor.r;
         self.g = newColor.g;
         self.b = newColor.b;
     }
 }
 
-struct Screen{
-    pub screen: Vec<Vec<ColorInfo>>,
+pub struct Screen{
+    pub screen: Vec<Vec<Color>>,
 }
 
 impl Screen{
-    fn new(x: usize, y: usize) -> Screen{
-        Screen{screen: vec![vec![ColorInfo::new(); x]; y]}
+    pub fn new(x: usize, y: usize) -> Screen{
+        Screen{screen: vec![vec![Color::new(); x]; y]}
     }
 
-    fn plot(&self, x: i32, y: i32, color: ColorInfo){
+    pub fn plot(&mut self, x: i32, y: i32, color: Color){
         if x >= 0 && y >= 0{
             self.screen[x as usize][y as usize].plotColor(color);
         }
     }
-}
-fn draw_line(x0: i32, y0: i32, x1: i32, y1: i32, screen: Screen, color: ColorInfo){
-    if x0 < x1{
-        let tmp = x0;
-        x0 = x1;
-        x1 = tmp;
-        tmp = y0;
-        y0 = y1;
-        y1 = tmp;
-    }
-    if y0 < y1{
-        // octant 2
-        if (y1-y0)/(x1-x0) > 1{
-            let x = x0;
-            let y = y0;
-            let a = 2*(y1-y0);
-            let b = -2*(x1-x0);
-            let d = a + 1/2*b;
-            while x <= x1{
-                screen.plot(x, y, color);
-                if d < 0{
-                    y += 1;
-                    d += b;
-                }
-                x += 1;
-                d += a;
-            }
-        }else{
-            // octant 1
-            let x = x0;
-            let y = y0;
-            let a = 2*(y1-y0);
-            let b = -2*(x1-x0);
-            let d = 1/2*a + b;
-            while y <= y1{
-                screen.plot(x, y, color);
-                if d < 0{
+
+    pub fn draw_line(&mut self, mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, color: Color){
+        if x0 < x1{
+            let mut tmp = x0;
+            x0 = x1;
+            x1 = tmp;
+            tmp = y0;
+            y0 = y1;
+            y1 = tmp;
+        }
+        if y0 < y1{
+            // octant 2
+            if (y1-y0)/(x1-x0) > 1{
+                let mut x = x0;
+                let mut y = y0;
+                let a = 2*(y1-y0);
+                let b = -2*(x1-x0);
+                let mut d = a + 1/2*b;
+                while x <= x1{
+                    self.plot(x, y, color);
+                    if d < 0{
+                        y += 1;
+                        d += b;
+                    }
                     x += 1;
                     d += a;
                 }
-                y += 1;
-                d += b;
-            }
-        }
-    }else{
-        //octant 7
-        if (y1-y0)/(x1-x0) > -1{
-            let x = x0;
-            let y = y0;
-            let a = 2*(y1-y0);
-            let b = -2*(x1-x0);
-            let d = a + 1/2*b;
-            while x <= x1{
-                screen.plot(x, y, color);
-                if d < 0{
-                    y -= 1;
+            }else{
+                // octant 1
+                let mut x = x0;
+                let mut y = y0;
+                let a = 2*(y1-y0);
+                let b = -2*(x1-x0);
+                let mut d = 1/2*a + b;
+                while y <= y1{
+                    self.plot(x, y, color);
+                    if d < 0{
+                        x += 1;
+                        d += a;
+                    }
+                    y += 1;
                     d += b;
                 }
-                x += 1;
-                d += a;
             }
         }else{
-            // octant 8
-            let x = x0;
-            let y = y0;
-            let a = 2*(y1-y0);
-            let b = -2*(x1-x0);
-            let d = 1/2*a + b;
-            while y <= y1{
-                screen.plot(x, y, color);
-                if d < 0{
-                    x -= 1;
+            //octant 7
+            if (y1-y0)/(x1-x0) > -1{
+                let mut x = x0;
+                let mut y = y0;
+                let a = 2*(y1-y0);
+                let b = -2*(x1-x0);
+                let mut d = a + 1/2*b;
+                while x <= x1{
+                    self.plot(x, y, color);
+                    if d < 0{
+                        y -= 1;
+                        d += b;
+                    }
+                    x += 1;
                     d += a;
                 }
-                y += 1;
-                d += b;
+            }else{
+                // octant 8
+                let mut x = x0;
+                let mut y = y0;
+                let a = 2*(y1-y0);
+                let b = -2*(x1-x0);
+                let mut d = 1/2*a + b;
+                while y <= y1{
+                    self.plot(x, y, color);
+                    if d < 0{
+                        x -= 1;
+                        d += a;
+                    }
+                    y += 1;
+                    d += b;
+                }
             }
         }
     }
